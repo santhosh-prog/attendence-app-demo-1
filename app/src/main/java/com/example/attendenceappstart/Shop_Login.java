b5832ede882e63094eef29ae6d48fb73000e1d3c
santhosh-prog <117562930+santhosh-prog@users.noreply.github.com>
2022-11-26 13:01:25 +0530

    shop login added

---
+++ app/src/main/java/com/example/attendenceappstart/Shop_Login.java
@@ -0,0 +1,14 @@
+package com.example.attendenceappstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shop_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_login2);
+    }
+ }
